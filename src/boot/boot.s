e9b6bc53d547bcad9fe3e7a87331f34975ba8080
0ZeroFive5 <artpoc04@gmail.com>
2025-10-29 21:25:22 +0100

    implemented drop down from el2 to el1

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -12,17 +12,67 @@
     add \register, \register, #:lo12:\symbol
 .endm
 
+
 .section .text._start
 
+/* All the CPU cores will jump to `_start` after the bootloader finishes */
 _start:
-    /* All the CPU cores will jump to `_start` after the bootloader finishes */
+
+    // park non boot cores (not needed because the rpi boot stub does it for us)
+    /*
     mrs x0, MPIDR_EL1
     and x0, x0, {CORE_ID_BITMASK}
     mov x1, {BOOT_CORE_ID}
     cmp x0, x1
+    */
+
+    // enable CNTP for EL1
+    mrs     x0, cnthctl_el2
+    orr     x0, x0, #3
+    msr     cnthctl_el2, x0
+    msr     cnthp_ctl_el2, xzr
+
+    // initialize virtual MPIDR
+    mrs     x0, midr_el1
+    mrs     x2, mpidr_el1
+    msr     vpidr_el2, x0
+    msr     vmpidr_el2, x2
+    // disable coprocessor traps
+    mov     x0, #0x33FF
+    msr     cptr_el2, x0
+    msr     hstr_el2, xzr
+    mov     x0, #(3 << 20)
+    msr     cpacr_el1, x0
+    // enable AArch64 in EL1
+    mov     x0, #(1 << 31)      // AArch64
+    orr     x0, x0, #(1 << 1)   // SWIO hardwired on Pi3
+    msr     hcr_el2, x0
+    mrs     x0, hcr_el2
+    // Setup SCTLR access
+    mov     x2, #0x0800
+    movk    x2, #0x30d0, lsl #16
+    msr     sctlr_el1, x2
+
+    ADR_REL x0, __boot_stack_end
+    msr     sp_el1, x0
+
+
 
     /* All non boot cores will jump into the parking loop */
     b.ne .L_parking_loop
+    /* drop down into el1 */
+    ldr x0, =0x30C50830 //all default values
+    msr SCTLR_EL1, x0
+
+  // drop to el1
+    ldr x0, =0x3C5 // set D A I F bits to mask all interrupts and stack to SP_EL1
+    msr SPSR_EL2, x0
+    adr x0, el1
+    msr ELR_EL2, x0
+    eret
+
+
+el1:
 
     ADR_REL x0, __bss_start
     ADR_REL x1, __bss_end
@@ -36,8 +86,6 @@ _start:
 
 .L_setup_rust:
     /* Set the stack pointer to the start of the boot stack */
-    ADR_REL x0, __boot_stack_end
-    mov sp, x0
 
     /* Jump into rust */
     b _start_rust
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ mod boot;
 mod cpu;
 mod driver;
 
+use aarch64_cpu::registers::{self, Readable};
 use core::panic::PanicInfo;
 use driver::uart0;
+use heapless::format;
 
 fn kernel_main() -> ! {
     uart0::init();
-    uart0::write_str("hi :3");
+    uart0::write_str("hi :3\n");
+    let el = Readable::get(&registers::CurrentEL);
+    uart0::write_str(format!(20; "current EL: {}\n", el >> 2).unwrap().as_str());
     panic!();
 }
 
