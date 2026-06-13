85426caf5956330a92fe1a473db58e604e6e02b3
maya <61090626+maiadere@users.noreply.github.com>
2026-06-13 17:59:19 +0200

    change vc mapping from device to normal memory

--- memory/src/mmu.rs
+++ memory/src/mmu.rs
@@ -276,7 +276,7 @@ pub unsafe fn create_initial_mappings() {
             0x3b40_0000 + UPPER_31_BITS,
             0x3b40_0000,
             0x4C0,
-            MemoryAttribute::Device,
+            MemoryAttribute::Memory,
             TT_access::Identity,
         );
     }
